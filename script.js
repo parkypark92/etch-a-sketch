6d3aa94c339a50ba86724692f0a1d27b65e868f5
Andrew <115462877+parkypark92@users.noreply.github.com>
2022-11-14 17:46:46 +0000

    add toggleFade() function to fade/no fade selection element

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const container = document.querySelector('.grid-container');
-const containerHeight = 800;
-setPixels(16);
+const containerHeight = 600;
+const fade = document.querySelector('#fade');
+fade.addEventListener('change', toggleFade)
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
     let num = prompt("Enter number of pixels");
     setPixels(num);
 });
-
+setPixels(16);
 
 function setPixels(num) {
     removePixels();
@@ -24,24 +25,37 @@ for(let i = 1; i <= numberOfPixels; i++)
         i.classList.remove('afterHover');
         i.classList.add('hovering');
     });
-    i.addEventListener('mouseout', () => {
-        i.classList.remove('hovering');
-        i.classList.add('afterHover');
-
-    });
+    
 }
 }
 
 
 function removePixels() 
 {
-    const pixels = document.querySelectorAll('.square');
+    let pixels = document.querySelectorAll('.square');
     pixels.forEach(pixel => pixel.remove());
 }
 
-
-
-
+function toggleFade()
+{
+    let pixels = document.querySelectorAll('.square');
+    if(fade.value === 'fade')
+    {
+        pixels.forEach(pixel => {
+         pixel.addEventListener('mouseout', () => {
+            pixel.classList.remove('hovering');
+            pixel.classList.add('afterHover');   
+        })
+        });
+    } else{
+        pixels.forEach(pixel => {
+            pixel.addEventListener('mouseout', () => {
+               pixel.classList.remove('afterHover');
+               pixel.classList.add('hovering');   
+           })
+           });
+    }
+}
 
 
 
