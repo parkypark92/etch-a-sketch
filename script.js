5445e2589dbd4c9fa6160732088183cbc473a4de
Andrew <115462877+parkypark92@users.noreply.github.com>
2022-11-16 18:11:53 +0000

    change rainbow mode to 10 random rgb values, tidy up code

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const containerHeight = 600;
 const defaultPixelSize = 48;
 let currentPixelNum;
 
+//buttons
 const gridSize = document.querySelector('#grid-size');
 gridSize.addEventListener('change', selectGridSize);
 
 const rainbow = document.querySelector('.rainbow');
 let rainbowSet = false;
 rainbow.addEventListener('click', applyRainbow);
-
 
 const toggleGrid = document.querySelector('.toggleGrid');
 toggleGrid.addEventListener('click', gridToggle);
@@ -23,13 +23,14 @@ document.body.onmousedown = () => mouseDown = true;
 document.body.onmouseup = () => mouseDown = false;
 
 
+//functions
 function draw(e) {
     if(e.type == 'mouseover' && !mouseDown) return;
     if(rainbowSet === false)
     {
     e.target.style.background = "rgb(0, 197, 197)";
     } else {
-        e.target.style.background = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+        e.target.style.background = `${randomRGB()}`;
     }
 }
 
@@ -111,9 +112,21 @@ function removePixels()
 }
 
 
-function randomNumber()
-{
-    return Math.floor(Math.random() * ((255 - 10) + 1)) + 10;
+function randomRGB() {
+    const rgbArray = ['rgba(255, 0, 0, 1)',
+        'rgba(255, 154, 0, 1)' ,
+        'rgba(208, 222, 33, 1)' ,
+        'rgba(79, 220, 74, 1)' ,
+        'rgba(63, 218, 216, 1)' ,
+        'rgba(47, 201, 226, 1)' ,
+        'rgba(28, 127, 238, 1)' ,
+        'rgba(95, 21, 242, 1)' ,
+        'rgba(186, 12, 248, 1)' ,
+        'rgba(251, 7, 217, 1)',
+        'rgba(255, 0, 0, 1)'];
+
+    return rgbArray[Math.floor(Math.random() * rgbArray.length)];
+
 }
 
 
